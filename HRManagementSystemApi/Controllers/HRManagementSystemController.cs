8edefb615f3f03ae88b4e75b9499b12a125eb722
AsmaRafiq <asmarafiq@live.com>
2017-08-09 12:27:09 +0500

    HRManagementSystemApi

--- HRManagementSystemApi/Controllers/HRManagementSystemController.cs
+++ HRManagementSystemApi/Controllers/HRManagementSystemController.cs
@@ -4,18 +4,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
 
 namespace HRManagementSystemApi.Controllers
 {
     public class HRManagementSystemController : ApiController
     {
-
+        HRManagementSystemEntities db = new HRManagementSystemEntities();
         [HttpGet]
         [Route("api/HRManagementSystem/EmployeeDetails")]
         public bool EmployeeDetails(InformationOfEmployee Information)
         {
-            return true;
+
+            var values = db.EmployeeDetails.ToList();
+            if (values != null)
+            {
+                foreach (var e in values)
+                {
+                    InformationOfEmployee emp = new InformationOfEmployee();
+                    emp.Id = e.Id;
+                    emp.Name = e.Name;
+                    emp.Post = e.Post;
+                    emp.Qualification = e.Qualification;
+                    emp.Skills = e.Skills;
+                    emp.Hiring = e.Hiring;
+                    emp.Description = e.Description;
+                    db.EmployeeDetails.Add(e);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+
+
+            else
+            {
+                return false;
+            }
         }
 
 
@@ -23,7 +48,26 @@ namespace HRManagementSystemApi.Controllers
         [Route("api/HRManagementSystem/EmployeePerformance")]
         public bool EmployeePerformance(InformationOfEmployee Information)
         {
-            return true;
+            var employee = db.EmployeeManagements.FirstOrDefault(e => e.EmployeeId ==  EmployeeId);
+            if (employee != null)
+            {
+                foreach (var e in employee)
+                {
+                    InformationOfEmployee emp = new InformationOfEmployee();
+                    emp.Id = e.Id;
+                    emp.Attendance = e.Name;
+                    emp.Leave = e.Discription;
+                    emp.ProjectDetails = e.StartDate;
+                    emp.Trainings = e.Trainings;
+                    emp.Apprasel = e.Apprasel;
+                    
+                    EmployeeDetail.Update(emp);
+                }
+                return true;
+            }
+           
+
+        
         }
 
 
@@ -34,5 +78,11 @@ namespace HRManagementSystemApi.Controllers
             return 0;
         }
 
+
+
+
+
+
+        public int? EmployeeId { get; set; }
     }
 }
--- HRManagementSystemApi/Models/InformationOfEmployee.cs
+++ HRManagementSystemApi/Models/InformationOfEmployee.cs
@@ -10,16 +10,19 @@ namespace HRManagementSystemApi.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Post { get; set; }
-        public float Salary { get; set; }
         public string Qualification { get; set; }
+        public string Skills { get; set; }
         public string Attendance { get; set; }
         public string Leave { get; set; }
-        public string ExpenseClaims { get; set; }
         public string Training { get; set; }
-        public string Interview { get; set; }
         public string hire { get; set; }
         public string fire { get; set; }
         public string ProjectDetails { get; set; }
-        public string Skills { get; set; }
+        public string ExpenseClaims { get; set; }
+        public float Salary { get; set; }
+        public int EmployeeId { get; set; }
+        public string Hiring { get; set; }
+
+        public string Description { get; set; }
     }
 }
